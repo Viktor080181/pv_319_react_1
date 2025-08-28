669fcfee70adb722e7a142471c777636b5e11dea
Viktor080181 <lozhnvikt0801@mail.ru>
2025-08-28 16:05:49 +0300

    DZ card

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -1,60 +1,31 @@
 import './Article.css';
-import gender from '../img/male.png'
 import male from '../img/male.png';
 import female from '../img/female.png';
 
-function Article(props)
-{
-  //let {title, content} = props;
-  let{db} = props;
-    return(
-        <article>
+function Article(props) {
+  let { db } = props;
 
-          <div className = 'app'>
-            {
-                Object.keys(db).map(elem => 
-                  {
-                  return(
-                    <div className='card' key = {elem}>
-                      <img src = {db[elem].photo} alt="Photo"/>
-                      <div className= 'name'>{db[elem].name}{db[elem].surname}</div>
-                      <div className= 'gender'> <img src={db[elem].pol=== 'male' ? male :female} alt = "Gender" /></div>
-                      <div className= 'age'> {db[elem].age}</div>
-                      </div>
-                         )
-             
-                   })
-            }
-            
-          {/* <div className = 'card'>
-            <img src = {db.photo} alt = "Photo"/>
-             <div className = 'name' >{db.name} {db.surname}</div>
-            <div className = 'gender'>
-            <img src={gender} alt = "Gender"/>
-          </div> */}
+  return (
+    <article>
+      <div className='app'>
+        {Object.keys(db).map(elem => (
+          <div className='card' key={elem}>
+            <img src={db[elem].photo} alt="Photo" />
+            <div className='name'>{db[elem].name} {db[elem].surname}</div>
+            <div className='gender'>
+              <img src={db[elem].pol === 'male' ? male : female} alt="Gender" />
+            </div>
+            <div className='age'>{db[elem].age}</div>
           </div>
-         
-       {/* </div></div><h1 style = {{title:"darkgreen"}}>{title}</h1> */}
-          {/* {content} */}
-     {/* <h2>Современные десктопные процессоры</h2> 
-      <p>– это ключевые компоненты персональных компьютеров, 
-        отвечающие за обработку данных и управление операциями. 
-        Они характеризуются высокой тактовой частотой, 
-        количеством ядер для параллельной обработки, а некоторые 
-        модели также включают встроенный графический процессор (iGPU). 
-        Кроме того, некоторые процессоры имеют разблокированный 
-        множитель для разгона. Важным аспектом 
-        является эффективное охлаждение для предотвращения перегрева
-
-      </p>*/}
-         
-      </article>
-    )
+        ))}
+      </div>
+    </article>
+  );
 }
-Article.defaultProps = 
-{
+
+Article.defaultProps = {
   title: "Заголовок статьи",
   content: "Содержимое статьи"
-}
+};
 
 export default Article;
